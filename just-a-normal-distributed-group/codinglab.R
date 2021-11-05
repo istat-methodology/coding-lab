8250450a6080c8ae0cf7da0756f8f2b1b4bd53e0
Jelena <34378793+jelena321@users.noreply.github.com>
2021-11-05 09:46:54 +0100

    Delete codinglab.R

--- just-a-normal-distributed-group/codinglab.R
+++
@@ -1,7 +0,0 @@
-library(readr)
-year2019 <- read_csv("full201952.dat")
-
-year2019 <- as.data.frame(year2019)
-plastic <-subset(year2019, PRODUCT_SITC>="57000"& PRODUCT_SITC<"59000")
-View(plastic)
-
